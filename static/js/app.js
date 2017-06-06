a46308d57a2dbc27bcdfd2e6550c5bb431d33f97
KrupMark <krupinczamark@hotmail.com>
2017-06-06 06:35:36 -0500

    plus gomb elkezdve...

---
+++ static/js/app.js
@@ -0,0 +1,22 @@
+﻿/*jshint esnext: true */
+/*jshint devel: true */
+/*jslint node: true */
+/*jslint browser: true */
+/*jslint jquery: true */
+
+function switchMenu(clickedId) {
+  console.log(clickedId+" megnyomva...");
+  // jquery selector: minden html elemet kiválaszt amin rajta van content class
+  $(".content").css('display', 'none');
+  $("#"+clickedId+".content").css('display', 'unset');
+
+  $(".menu-item").removeClass("btn-primary");
+
+  $("#"+clickedId+".menu-item").removeClass("btn-secondary");
+  $("#"+clickedId+".menu-item").addClass("btn-primary");
+}
+function addMunkanap() {
+    $('#munkanapItems').append(`
+    <div>HELLO</div>
+    `);
+}
